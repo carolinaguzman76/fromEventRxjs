ad36945fa7c314a86ba0c59e020fbb8420f552a9
DESKTOP-BE96BQH\carol <carolinaguzman76@gmail.com>
2020-09-21 19:17:51 +0200

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fromEventRxjs';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const el = document.getElementById('elemento');
+
+    const mouseMove = fromEvent(el, 'mousemove');
+
+    mouseMove.subscribe((e: MouseEvent) => {
+      console.log(`Coords: X: ${e.clientX}, Y: ${e.clientY}`);
+    });
+  }
 }
